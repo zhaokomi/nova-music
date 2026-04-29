11b50afdb134b5a94d17c35021f731fac93385f6
星之墨辰 <163753321+zhaokomi@users.noreply.github.com>
2026-04-30 02:49:32 +0800

    fix: error recovery, lyrics, v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.novamusic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
