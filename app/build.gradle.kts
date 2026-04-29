4dfd7e14e5f45ba65f6228f62aab56ad96f1225d
星之墨辰 <163753321+zhaokomi@users.noreply.github.com>
2026-04-29 23:09:48 +0800

    chore: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.novamusic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
