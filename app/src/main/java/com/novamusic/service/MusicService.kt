64c636a83e5ccef9dd359bc90579f746126fe808
星之墨辰 <163753321+zhaokomi@users.noreply.github.com>
2026-04-29 20:17:39 +0800

    fix: add flow.update import

--- app/src/main/java/com/novamusic/service/MusicService.kt
+++ app/src/main/java/com/novamusic/service/MusicService.kt
@@ -27,6 +27,7 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.random.Random
